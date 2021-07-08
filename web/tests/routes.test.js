ba940fed835f4a0e78edcd5f2016a7c95576d541
Bassem Dghaidi <bassem@interop.link>
2021-07-08 16:29:04 +0200

    Fix linting issues

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -1,13 +1,13 @@
-const request = require('supertest');
-const app = require('../app');
+const request = require('supertest')
+const app = require('../app')
 
 describe('Default Endpoint', () => {
   it('should return status 200', async () => {
     const res = await request(app)
-      .get('/');
-    expect(res.statusCode).toEqual(200);
-  });
-});
+      .get('/')
+    expect(res.statusCode).toEqual(200)
+  })
+})
 
 // describe('New Endpoint', () => {
 //   it('should return a static json response with status 200', async () => {
