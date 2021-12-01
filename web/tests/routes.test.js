bc52f4e1605d17dd3bc6f02798be79fe866f5ea6
Bassem Dghaidi <bassem@interop.link>
2021-12-01 21:05:00 +0100

    Fix unit tests and update dependencies

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -15,7 +15,7 @@ describe('New Endpoint', () => {
       .get('/ice-flakes')
     expect(res.body).toEqual({
       resource: 'ice-flakes',
-      count: 100,
+      count: 102,
       shape: 'circle'
     })
     expect(res.statusCode).toEqual(201)
