eccf0858470b9a218336bcc05feef5e1e5ec5e23
Bassem Dghaidi <link-@github.com>
2021-12-01 21:17:16 +0100

    Fix for compatibility with ubuntu version

--- infra/instances/staging/create_staging_resource.sh
+++ infra/instances/staging/create_staging_resource.sh
@@ -69,7 +69,7 @@ _main() {
     "${local_dir}/extra_staging_${random_id}.tf"
 
   # Replace the <RANDOMID> with the actual random identifier
-  sed -i '' "s/RANDOMID/${random_id}/g" "${local_dir}/extra_staging_${random_id}.tf"
+  sed -i "s/RANDOMID/${random_id}/g" "${local_dir}/extra_staging_${random_id}.tf"
 
   # Exit with a success status
   echo '{"resource_file": "extra_staging_'${random_id}'.tf", "terraform_expected_output": "staging_dns_'${random_id}'"}'
