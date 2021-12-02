aa8a723408e87a11d1d65f83ae7d4ea88b447875
Bassem Dghaidi <bassem@interop.link>
2021-12-02 20:26:13 +0100

    Fix 2

--- web/routes/index.js
+++ web/routes/index.js
@@ -12,7 +12,7 @@ router.get('/ice-flakes', function (req, res, next) {
     .status(201)
     .json({
       resource: 'ice-flakes',
-      count: 109,
+      count: 110,
       shape: 'circle'
     })
 })
