a9f8fb8452a340f97fdb42cb5c72db735cbbe016
Bassem Dghaidi <bassem@interop.link>
2021-12-02 20:19:29 +0100

    Fix

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -15,7 +15,7 @@ describe('New Endpoint', () => {
       .get('/ice-flakes')
     expect(res.body).toEqual({
       resource: 'ice-flakes',
-      count: 108,
+      count: 109,
       shape: 'circle'
     })
     expect(res.statusCode).toEqual(201)
