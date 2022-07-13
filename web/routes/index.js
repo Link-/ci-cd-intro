67037ba43e5f214513e93c7e6ba6920bab25ffde
Bassem Dghaidi <bassem@interop.link>
2022-07-13 12:52:53 +0000

    Updating the ice-flakes route

--- web/routes/index.js
+++ web/routes/index.js
@@ -12,7 +12,7 @@ router.get('/ice-flakes', function (req, res, next) {
     .status(201)
     .json({
       resource: 'ice-flakes',
-      count: 205,
+      count: 300,
       shape: 'circle'
     })
 })
