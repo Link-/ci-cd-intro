47ba098ce0b452bb4008d628288758309f94ac30
Afaq-DevOps <115092188+Afaq-DevOps@users.noreply.github.com>
2023-03-29 12:36:53 +0500

    Update users.js

--- web/routes/users.js
+++ web/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/users', function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource')
 })
 
