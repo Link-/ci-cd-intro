c473aa5b930b32c34071d7daa3b1d2c80c693c34
Piyush Kumar <20BCS9107@cuchd.in>
2023-04-01 16:49:41 +0530

    Update users.js

--- web/routes/users.js
+++ web/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
-  res.send('respond with a resource')
+  res.send('respond with a resource and commit')
 })
 
 module.exports = router
