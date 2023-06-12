adb6a5acfddf70cb767b94e1bde4d9d3c05e7528
DIALLO ABOUBACAR <58235631+aboubacar3012@users.noreply.github.com>
2023-06-12 12:31:41 +0200

    Update index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function (req, res, next) {
 /* GET ice-flakes resource */
 router.get('/ice-flakes', function (req, res, next) {
   res
-    .status(201)
+    .status(200)
     .json({
       resource: 'ice-flakes',
       count: 205,
