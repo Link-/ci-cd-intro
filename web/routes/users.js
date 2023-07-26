2b7f5c40a7e2c4999894fbbefbef42386f1dedef
Kedar-123 <97400367+Kedar-123@users.noreply.github.com>
2023-07-26 12:59:28 +0530

    Update users.js

--- web/routes/users.js
+++ web/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET users listing. */
+/* GET all users listing. */
 router.get('/users', function (req, res, next) {
   res.send('respond with a resource')
 })
