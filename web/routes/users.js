c986a7b0a3915001656674d501113cee1bd45c46
Piyush_vasandani-at-781613858829 <vasandanipiyush@gmail.com>
2023-10-29 18:44:21 +0530

    my-protection-test

--- web/routes/users.js
+++ web/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/myusers', function (req, res, next) {
+router.get('/myuse', function (req, res, next) {
   res.send('respond with a resource')
 })
 
