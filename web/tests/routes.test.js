72db10a13d9e58d85d808aa2af9ffd80c7c8f0b7
Ridzwan <4947140+omgsian@users.noreply.github.com>
2023-10-31 07:03:36 +0000

    Fix unit tests

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -16,7 +16,7 @@ describe('New Endpoint', () => {
     expect(res.body).toEqual({
       resource: 'ice-flakes',
       count: 205,
-      shape: 'rectangle'
+      shape: 'circle'
     })
     expect(res.statusCode).toEqual(201)
   })
