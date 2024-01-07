ae0f317e280c580e34711d9f201a15cb424bd0ad
Tendai Makuwerere <54901263+ttendaim@users.noreply.github.com>
2024-01-07 01:53:57 +0100

    Update routes.test.js

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -15,7 +15,7 @@ describe('New Endpoint', () => {
       .get('/ice-flakes')
     expect(res.body).toEqual({
       resource: 'ice-flakes',
-      count: 1005,
+      count: 105,
       shape: 'circle'
     })
     expect(res.statusCode).toEqual(201)
