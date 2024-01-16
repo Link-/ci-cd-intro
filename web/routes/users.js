24de4fa7dfac0bc61d5222fce81601edaf468d41
Amit Kumar <amit.kumar@rapidops.com>
2024-01-16 12:25:04 +0530

    new chnage for users

--- web/routes/users.js
+++ web/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
-  res.send('respond with a resource')
+  res.send('respond with a resource by amit')
 })
 
 module.exports = router
