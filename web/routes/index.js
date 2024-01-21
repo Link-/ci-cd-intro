833500713d9b915832223c81b339f94352dbef3f
Bertrand_Yangni <91333544+BertrandYangni@users.noreply.github.com>
2024-01-21 11:07:10 +0000

    Update index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -12,7 +12,7 @@ router.get('/ice-flakes', function (req, res, next) {
     .status(201)
     .json({
       resource: 'ice-flakes',
-      count: 1005,
+      count: 205,
       shape: 'rectangle'
     })
 })
