9bb75b0f4ebc401c5d89805b248926ba79fd9a06
root <root@DESKTOP-2I1UI4J>
2024-03-05 16:23:40 +0530

    added a new route tanaya

--- web/app.js
+++ web/app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.get('/tanaya').then(console.log("working"))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
