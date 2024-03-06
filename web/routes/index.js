ca0ba1a87cd0167a6ee22fcca77cac97d8322d2a
Michael Hy <109703743+HyMike@users.noreply.github.com>
2024-03-06 14:56:15 -0800

    Update index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -12,8 +12,8 @@ router.get('/ice-flakes', function (req, res, next) {
     .status(201)
     .json({
       resource: 'ice-flakes',
-      count: 1005,
-      shape: 'rectangle'
+      count: 205,
+      shape: 'circle'
     })
 })
 
